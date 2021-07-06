bcca7a54742d945849ee7c52ddbd432c8bd3f509
Alexander Kalabin <74727055+K414BIN@users.noreply.github.com>
2021-07-06 17:53:07 +0300

    Revert "Homework  for the first lesson"

--- hw1_1.js
+++
@@ -1,4 +0,0 @@
-var Tc = prompt("Enter the temperature in degrees Celsius");
-Tc=parseInt(Tc);
-var Tf = (9 / 5) * Tc + 32;
-alert ("Temperature in degrees Fahrenheit are : " + Tf);
--- hw1_2.js
+++
@@ -1,3 +0,0 @@
-var name="Василий";
-var admin=name.slice(0);
-alert (admin);
