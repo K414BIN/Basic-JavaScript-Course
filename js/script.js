b637a7b733d74d3c5d2138092f4fa9a29cdde5e9
Alexander Kalabin <k414bin@yandex.ru>
2021-08-03 13:56:02 +0300

    Fix an error in style.css !

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$('document').ready(function() {
+    loadGoods();
+});
+
+function loadGoods() {
+    //загрузка списка товаров
+   $.getJSON('goods.json', function(data) {
+       console.log(data);
+        var out="";
+       for ( var key in data) {
+              out += '<div class="single-goods" id="' + data[key]['Code']+ '">';
+              out += '<h3>' +  data[key]['Name'] +'</h3>';
+              out += '<p>Description: ' +  data[key]['Description'] +'</p>';
+              out += '<img class="image" src="'+data[key]['Image'] +'"/>';
+              out += '<p>Price: ' +  data[key]['Price'] +'</p>';
+              out += '<button id="add-to-cart">Buy</button>';
+              out += '<button id="del-from-cart">Cancel</button>';
+              out += '</div>';
+       }
+        $('#merchandise').html(out);
+       
+   })
+}
